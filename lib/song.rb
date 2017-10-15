dd2e0f224f21cdcdb87506ee157ff03524cd7173
Dominic Chapman <dominicchapman@me.com>
2017-10-15 16:08:01 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,66 @@
+class Song
+
+  @@all = []
+  attr_accessor :name
+  attr_reader :artist, :genre
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(song_name)
+    song = self.new(song_name)
+    song.save
+    song
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+
+  def self.find_by_name(name)
+    all.detect{ |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
+
+  def self.new_from_filename(filename)
+    split = filename.split(' - ')
+
+    artist_name = split[0]
+    song_name = split[1]
+    genre_name = split[2].split('.')[0]
+
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+
+    new(song_name, artist, genre)
+  end
+
+  def self.create_from_filename(filename)
+    new_from_filename(filename).tap{ |song| song.save }
+  end
+
+end
